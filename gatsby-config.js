263dd05fcbf8c6939ddfa308f935ea696f826de2
Jeff Bliss <jeff.a.bliss@gmail.com>
2020-07-13 11:05:57 -0400

    added a 404 error page

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,17 +33,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Typography from '@material-ui/core/Typography';
+import Layout from "../components/layout"
+
+const NotFoundPage = () => (
+  <Layout>
+    <Typography>
+      Sorry we cannot find what you're looking for. Please go back and try again.
+    </Typography>
+  </Layout>
+)
+
+export default NotFoundPage
